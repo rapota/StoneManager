25afc2d82023acdca2c1691499037fd5d43f5c34
Oleksandr Rapota <rapota84@gmail.com>
2023-08-20 03:44:25 -0700

    Added private API.

---
+++ src/StoneManager/Controllers/StoneController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using StoneMessages;
+
+namespace StoneManager.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StoneController : ControllerBase
+{
+    private readonly ILogger<StoneController> _logger;
+
+    public StoneController(ILogger<StoneController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<List<Stone>> Get([FromQuery] int? count, CancellationToken ct)
+    {
+        if (count >= 10)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (count >= 5)
+        {
+            await Task.Delay(
+                TimeSpan.FromSeconds(count.GetValueOrDefault()),
+                ct);
+        }
+
+        List<Stone> stones = Enumerable.Range(1, count ?? 3)
+            .Select(_ => new Stone
+            {
+                Id = Guid.NewGuid(),
+                Weight = Random.Shared.Next(0, 100)
+            })
+            .ToList();
+
+        return stones;
+    }
+}
